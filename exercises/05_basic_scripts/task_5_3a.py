182c85aa23734cdb6fa8bba3aa0ffd4ad22305f5
impossible-possible <deykoa@gmail.com>
2019-08-29 09:58:54 +0000

    5.3a done

---
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 5.3a
+
+Дополнить скрипт из задания 5.3 таким образом, чтобы, в зависимости от выбранного режима,
+задавались разные вопросы в запросе о номере VLANа или списка VLANов:
+* для access: 'Введите номер VLAN:'
+* для trunk: 'Введите разрешенные VLANы:'
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+То есть эту задачу можно решить без использования условия if и циклов for/while.
+'''
+
+access_template = [
+    'switchport mode access', 'switchport access vlan {}',
+    'switchport nonegotiate', 'spanning-tree portfast',
+    'spanning-tree bpduguard enable'
+]
+
+trunk_template = [
+    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
+    'switchport trunk allowed vlan {}'
+]
+
+mode = input('Enter interface mode (access/trunk): ')
+slov1 ={'access':'Enter number of VLAN: ','trunk':'Enter allowed VLANS: '}
+inter = input('Enter interface type and number: ')
+vlan = input(slov1[mode])
+
+slov ={'access':access_template,'trunk':trunk_template}
+print('interface {}'.format(inter))
+print('\n'.join(slov[mode]).format(vlan))
