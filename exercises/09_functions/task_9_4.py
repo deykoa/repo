848625aaf183509bd2675cec2d561d1da53c73af
impossible-possible <deykoa@gmail.com>
2019-09-06 10:40:05 +0000

    9.4 done

---
+++ exercises/09_functions/task_9_4.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 9.4
+
+Создать функцию convert_config_to_dict, которая обрабатывает конфигурационный файл коммутатора и возвращает словарь:
+* Все команды верхнего уровня (глобального режима конфигурации), будут ключами.
+* Если у команды верхнего уровня есть подкоманды, они должны быть в значении у соответствующего ключа, в виде списка (пробелы в начале строки надо удалить).
+* Если у команды верхнего уровня нет подкоманд, то значение будет пустым списком
+
+У функции должен быть один параметр config_filename, который ожидает как аргумент имя конфигурационного файла.
+
+Проверить работу функции на примере файла config_sw1.txt
+
+При обработке конфигурационного файла, надо игнорировать строки, которые начинаются с '!',
+а также строки в которых содержатся слова из списка ignore.
+
+Для проверки надо ли игнорировать строку, использовать функцию ignore_command.
+
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+ignore_l = ['duplex', 'alias', 'Current configuration']
+
+
+def ignore_command(command, ignore):
+    '''
+    Функция проверяет содержится ли в команде слово из списка ignore.
+
+    command - строка. Команда, которую надо проверить
+    ignore - список. Список слов
+
+    Возвращает
+    * True, если в команде содержится слово из списка ignore
+    * False - если нет
+    '''
+    for ign in ignore:
+        if ign in command or command.startswith('!'):
+            return True
+    return False
+def convert_config_to_dict(config_filename):
+    with open(config_filename) as f:
+        conf={}
+        for line in f:
+            if not ignore_command(line,ignore_l):
+                if not line.startswith(' '):
+                    a=line.strip()
+                    conf[line.strip()]=[]
+                    mark=True
+                else:
+                    conf[a].append(line.strip())
+    return conf
+
+print(convert_config_to_dict('config_sw1.txt'))
+
