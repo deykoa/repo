6f64809fb511deb7862c87f431753ec543ce9e3f
impossible-possible <deykoa@gmail.com>
2019-09-06 07:42:54 +0000

    9.2a done

---
+++ exercises/09_functions/task_9_2a.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 9.2a
+
+Сделать копию функции generate_trunk_config из задания 9.2
+
+Изменить функцию таким образом, чтобы она возвращала не список команд, а словарь:
+    - ключи: имена интерфейсов, вида 'FastEthernet0/1'
+    - значения: список команд, который надо выполнить на этом интерфейсе
+
+Проверить работу функции на примере словаря trunk_config и шаблона trunk_mode_template.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+
+trunk_mode_template = [
+    'switchport mode trunk', 'switchport trunk native vlan 999',
+    'switchport trunk allowed vlan'
+]
+
+trunk_config = {
+    'FastEthernet0/1': [10, 20, 30],
+    'FastEthernet0/2': [11, 30],
+    'FastEthernet0/4': [17]
+}
+def generate_trunk_config(trank_conf,trunk_temp):
+    confint={}
+    for intf,vlans in trank_conf.items():
+        conf=[]
+        for command in trunk_temp:
+            if 'allowed vlan' in command:
+                conf.append(command+' '+','.join([str(v) for v in vlans]))
+            else: conf.append(command)
+        confint[intf]=conf
+    return confint
+
+trunk_mode_template = [
+    'switchport mode trunk', 'switchport trunk native vlan 999',
+    'switchport trunk allowed vlan'
+]
+
+trunk_config = {
+    'FastEthernet0/1': [10, 20, 30],
+    'FastEthernet0/2': [11, 30],
+    'FastEthernet0/4': [17]
+}
+print(generate_trunk_config(trunk_config,trunk_mode_template))
