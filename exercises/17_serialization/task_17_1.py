b2c9c307895b88fccda5f1e7d140f3113008f542
impossible-possible <deykoa@gmail.com>
2019-11-06 08:56:56 +0000

    17.1 done

---
+++ exercises/17_serialization/task_17_1.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 17.1
+
+В этом задании нужно:
+* взять содержимое нескольких файлов с выводом команды sh version
+* распарсить вывод команды с помощью регулярных выражений и получить информацию об устройстве
+* записать полученную информацию в файл в CSV формате
+
+Для выполнения задания нужно создать две функции.
+
+Функция parse_sh_version:
+* ожидает как аргумент вывод команды sh version одной строкой (не имя файла)
+* обрабатывает вывод, с помощью регулярных выражений
+* возвращает кортеж из трёх элементов:
+ * ios - в формате "12.4(5)T"
+ * image - в формате "flash:c2800-advipservicesk9-mz.124-5.T.bin"
+ * uptime - в формате "5 days, 3 hours, 3 minutes"
+
+У функции write_inventory_to_csv должно быть два параметра:
+ * data_filenames - ожидает как аргумент список имен файлов с выводом sh version
+ * csv_filename - ожидает как аргумент имя файла (например, routers_inventory.csv), в который будет записана информация в формате CSV
+* функция записывает содержимое в файл, в формате CSV и ничего не возвращает
+
+
+Функция write_inventory_to_csv должна делать следующее:
+* обработать информацию из каждого файла с выводом sh version:
+ * sh_version_r1.txt, sh_version_r2.txt, sh_version_r3.txt
+* с помощью функции parse_sh_version, из каждого вывода должна быть получена информация ios, image, uptime
+* из имени файла нужно получить имя хоста
+* после этого вся информация должна быть записана в CSV файл
+
+В файле routers_inventory.csv должны быть такие столбцы:
+* hostname, ios, image, uptime
+
+В скрипте, с помощью модуля glob, создан список файлов, имя которых начинается на sh_vers.
+Вы можете раскомментировать строку print(sh_version_files), чтобы посмотреть содержимое списка.
+
+Кроме того, создан список заголовков (headers), который должен быть записан в CSV.
+'''
+
+import glob
+import re
+import csv
+
+sh_version_files = glob.glob('sh_vers*')
+#print(sh_version_files)
+
+headers = ['hostname', 'ios', 'image', 'uptime']
+
+def parse_sh_version(show_v):
+    matches = re.search(r'Software \S+, Version (\S+),.*uptime is (.*?)\n.*file is (\S+)', show_v, re.S)
+    return (matches.group(1),matches.group(3),matches.group(2))
+
+def write_inventory_to_csv(data_filenames, csv_filename):
+    result=[]
+    for sh_file in data_filenames:
+        with open(sh_file,) as f:
+            result.append([(sh_file.split('_')[-1].split('.')[0]),]+list(parse_sh_version(f.read())))
+    with open(csv_filename,'w') as ff:
+        write=csv.writer(ff)
+        write.writerow(headers)
+        write.writerows(result)
+
+if __name__=='__main__':
+    with open('sh_version_r1.txt') as f:
+        data=f.read()
+        #print(parse_sh_version(data))
+    
+    write_inventory_to_csv(sh_version_files,'test.csv')
+
