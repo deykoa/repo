567f36011b2664ca7b79def2d54c71cc79ed0ed3
impossible-possible <deykoa@gmail.com>
2019-12-27 13:07:18 +0000

    19.2c done

---
+++ exercises/19_ssh_telnet/task_19_2c.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 19.2c
+
+Скопировать функцию send_config_commands из задания 19.2b и переделать ее таким образом:
+
+Если при выполнении команды возникла ошибка,
+спросить пользователя надо ли выполнять остальные команды.
+
+Варианты ответа [y]/n:
+* y - выполнять остальные команды. Это значение по умолчанию, поэтому нажатие любой комбинации воспринимается как y
+* n или no - не выполнять остальные команды
+
+Функция send_config_commands по-прежнему должна возвращать кортеж из двух словарей:
+* первый словарь с выводом команд, которые выполнились без ошибки
+* второй словарь с выводом команд, которые выполнились с ошибками
+
+Оба словаря в формате
+* ключ - команда
+* значение - вывод с выполнением команд
+
+Проверить работу функции можно на одном устройстве.
+
+Пример работы функции:
+
+In [11]: result = send_config_commands(r1, commands)
+Подключаюсь к 192.168.100.1...
+Команда "logging 0255.255.1" выполнилась с ошибкой "Invalid input detected at '^' marker." на устройстве 192.168.100.1
+Продолжать выполнять команды? [y]/n: y
+Команда "logging" выполнилась с ошибкой "Incomplete command." на устройстве 192.168.100.1
+Продолжать выполнять команды? [y]/n: n
+
+In [12]: pprint(result)
+({},
+ {'logging': 'config term\n'
+             'Enter configuration commands, one per line.  End with CNTL/Z.\n'
+             'R1(config)#logging\n'
+             '% Incomplete command.\n'
+             '\n'
+             'R1(config)#',
+  'logging 0255.255.1': 'config term\n'
+                        'Enter configuration commands, one per line.  End with '
+                        'CNTL/Z.\n'
+                        'R1(config)#logging 0255.255.1\n'
+                        '                   ^\n'
+                        "% Invalid input detected at '^' marker.\n"
+                        '\n'
+                        'R1(config)#'})
+
+'''
+
+# списки команд с ошибками и без:
+commands_with_errors = ['logging 0255.255.1', 'logging', 'i']
+correct_commands = ['logging buffered 20010', 'ip http server']
+
+commands = commands_with_errors + correct_commands
+
+import netmiko
+import yaml
+
+def send_show_command(device,commands,verbose=True):
+    if verbose:
+        print(f'Connect to {device["ip"]}...')
+    bad,good = {},{}
+    with netmiko.ConnectHandler(**device) as ssh:
+        ssh.enable()
+        for command in commands:
+            res = ssh.send_config_set(command)
+            if 'Invalid input detected' in res:
+                bad[command]=res
+                print(command,f'''falied with error "Invalid input detected" on device {device['ip']}''')
+                req=input('continue rest commands? y/n ')
+                if (req=='n') or (req =='no'):
+                    break
+            elif 'Incomplete command' in res:
+                bad[command]=res
+                print(command,f'''falied with error "Incomlete command" on device {device['ip']}''')
+                req=input('continue rest commands? y/n ')
+                if (req=='n') or (req =='no'):
+                    break
+            elif 'Ambiguous command' in res:
+                bad[command]=res
+                print(command,f'''falied with error "Ambigous command" on device {device['ip']}''')
+                req=input('continue rest commands? y/n ')
+                if (req=='n') or (req =='no'):
+                    break
+
+            else:
+                good[command]=res
+        
+    return (bad, good)
+
+with open('devices.yaml') as f:
+    devices = yaml.safe_load(f)
+    for device in devices:
+        print(send_show_command(device,commands))
+        print('-'*20)
+
