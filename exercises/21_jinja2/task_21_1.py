2a3148c9927c10331716f15d4a3f833b6cc5333c
Vagrant Default User <vagrant@stretch.localdomain>
2020-02-12 12:13:59 +0000

    version with environment

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -13,16 +13,15 @@
 Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
 
 '''
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 import yaml
 
 def generate_config(template,data_dict):
-    with open(template) as t:
-        temp=t.read()
-        template=Template(temp)
+    env=Environment(loader=FileSystemLoader('templates'))
+    template=env.get_template(template)
     with open(data_dict) as f:
         dict_vars=yaml.safe_load(f)
     return template.render(dict_vars)
 
 if __name__=='__main__':
-    print(generate_config('templates/for.txt','data_files/for.yml'))
+    print(generate_config('for.txt','data_files/for.yml'))
